79318d2e8f76f42f577349d586b4b1749b4b8e7a
Stas Sukora <st.sukora@gmail.com>
2022-11-21 21:59:39 +0300

    Update readme for stage0-module4-conditions-task1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int falseNumber = -10;
+        if (falseNumber > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int trueNumber = 10;
+        if (trueNumber > 0) {
+            System.out.println(true);
+        }
     }
 }
